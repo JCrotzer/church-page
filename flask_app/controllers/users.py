74a0721c8e70d903002a6d6bb369ca17622b56ac
Julie Crotzer <jcrotzerrn@aol.com>
2022-09-16 17:37:11 -0500

    updated homepage.html and css

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,4 +1,40 @@
 from flask_app import app
-from flask import render_template, redirect, request, session,
-flash
+from flask import render_template, redirect, request, session,flash
 from flask_app.models.user import User
+
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
+# CREATE 
+
+@app.route('/users/register', methods=['POST'])
+def register_user():
+
+    if not User.validate_register(request.form):
+        return redirect('/users/register')
+    data = {
+        "first_name": request.form['first_name'],
+        "last_name": request.form['last_name'],
+        "email": request.form['email'],
+        "password": bcrypt.generate_password_hash(request.form['password'])
+    }
+    session['user_id'] = User.create_user(data)
+    return redirect('/users/account')
+
+@app.route('/users/login', methods=['POST'])
+def login_user():
+    this_user = User.read_by_email({"email": request.form['email']})
+    if not this_user:
+        flash("Invalid Email/Password")
+        return redirect('/')
+    if not bcrypt.check_password_hash(this_user.password, request.form['password']):
+        flash("Invalid Email/Password")
+        return redirect('/')
+    session['user_id'] = this_user.id
+    return redirect('/users/account')
+
+# READ 
+
+@app.route('/')
+def index():
+    return render_template("homepage.html")
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -1,1 +1,23 @@
 from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app import app
+from flask import flash, session
+
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_0]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
+class User:
+    db = "fbchurch"
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+# CREATE
+
+    
