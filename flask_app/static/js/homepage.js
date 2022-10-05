9a4348ff6b516017c3d7e76dbc3bf282198df09f
Julie Crotzer <jcrotzerrn@aol.com>
2022-10-04 19:52:38 -0500

    updated homepage.html and css

---
+++ flask_app/static/js/homepage.js
@@ -0,0 +1,42 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items: 1,
+    loop:true,
+    margin:10,
+    // nav:true,
+    autoplay: true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        875:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+$(document).ready(function() {
+    //responsive menu toggle
+    $("#menutoggle").click(function() {
+        $('.xs-menu').toggleClass('displaynone');
+
+        });
+    //add active class on menu
+    $('ul li').click(function(e) {
+        e.preventDefault();
+        $('li').removeClass('active');
+        $(this).addClass('active');
+    });
+//drop down menu	
+        $(".drop-down").hover(function() {
+            $('.mega-menu').addClass('display-on');
+        });
+        $(".drop-down").mouseleave(function() {
+            $('.mega-menu').removeClass('display-on');
+        });
+});
